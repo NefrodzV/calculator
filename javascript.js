8a15eb8a5f7d8c6acc02ae9ab258df779612849b
Neftaly <neftalyrv23@gmail.com>
2023-04-15 18:54:15 -0400

    Added basic functions that will be used and variables

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// Variable that will be used
+let x;
+let y;
+let symbol;
+
+const add = function() {
+
+}
+
+const substract = function() {
+
+}
+
+const multiply = function() {
+
+}
+
+const divide = function() {
+
+}
+
+const operate = function(int) {
+    return int;
+}
+
