67c2a180607179a1568a346cbfe6e4d4e122afab
Neftaly <neftalyrv23@gmail.com>
2023-04-16 12:10:05 -0400

    Added a switch statement that will handle button events.

--- javascript.js
+++ javascript.js
@@ -30,13 +30,87 @@ const updateDisplay = function() {
 }
 updateDisplay();
 
+function keyEvent(id) {
+
+    switch (id) {
+        case "zero":
+            console.log("ZERO WAS CLICKED");
+            break;
+
+        case "one":
+            console.log("ONE WAS CLICKED");
+            break;
+
+        case "two":
+            console.log("TWO WAS CLICKED");
+            break;
+            
+        case "three":
+            console.log("THREE WAS CLICKED");
+            break;
+
+        case "four":
+            console.log("FOUR WAS CLICKED");
+            break;
+
+        case "five":
+            console.log("FIVE WAS CLICKED");
+            break;
+
+        case "six":
+            console.log("SIX WAS CLICKED");
+            break;
+
+        case "seven":
+            console.log("SEVEN WAS CLICKED");
+            break;
+
+        case "eigth":
+            console.log("EIGHT WAS CLICKED");
+            break;
+
+        case "nine":
+            console.log("NINE WAS CLICKED");
+            break;
+
+        case "add":
+            console.log("ADD WAS CLICKED");
+            break;
+        
+        case "minus":
+            console.log("MINUS WAS CLICKED");
+            break;
+
+        case "divide":
+            console.log("DIVIDE WAS CLICKED");
+            break;
+
+        case "multiply":
+            console.log("MULTIPLY WAS CLICKED");
+            break;
+
+        
+        case "clear":
+            console.log("CLEAR WAS CLICKED");
+            break;
+        case "equals":
+            console.log("EQUALS WAS CLICKED");
+            break;
+
+        default:
+            console.log("UH OH!")
+            break;
+    }
+}
+
 let buttons = document.querySelectorAll(".key");
 // Adding event listeners to buttons
 Array.from(buttons).forEach(button => {
-    button.addEventListener('click', function(e){
-        console.log(this.id);
-        console.log(this.textContent);
+    button.addEventListener('click', function(e) {
+        keyEvent(this.id);
     })
 })
+
+
 
 
