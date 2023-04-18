3f9f9adcd4323ff2cd6ca34d493b0889c2af4e8d
Neftaly <neftalyrv23@gmail.com>
2023-04-18 17:04:40 -0400

    Added a switch statement to button eventlisteners to update the current onscreen value and added the button clear all to reset all values

--- javascript.js
+++ javascript.js
@@ -1,113 +1,59 @@
 // Variable that will be used
-let x = 0;
-let y = 0;
-let symbol = 0;
-let total = 0;
+let currentNumber = "0";
+let previousNumber = null;
+let symbol = null;
 
-const add = function() {
-
+let operate = function() {
+    console.log("Run operation with " + symbol);
 }
 
-const substract = function() {
+let total = document.querySelector("#total");
 
+const update = function() {
+    total.textContent = currentNumber;
 }
 
-const multiply = function() {
-
+update();
+const keyEvent  = function(key) {
+   
 }
-
-const divide = function() {
-
-}
-
-const operate = function(int) {
-    return int;
-}
-
-const DISPLAY = document.getElementById("total");
-const updateDisplay = function() {
-    DISPLAY.textContent = total;
-}
-updateDisplay();
-
-function keyEvent(id) {
-
-    switch (id) {
-        case "zero":
-            console.log("ZERO WAS CLICKED");
-            break;
-
-        case "one":
-            console.log("ONE WAS CLICKED");
-            break;
-
-        case "two":
-            console.log("TWO WAS CLICKED");
-            break;
-            
-        case "three":
-            console.log("THREE WAS CLICKED");
-            break;
-
-        case "four":
-            console.log("FOUR WAS CLICKED");
-            break;
-
-        case "five":
-            console.log("FIVE WAS CLICKED");
-            break;
-
-        case "six":
-            console.log("SIX WAS CLICKED");
-            break;
-
-        case "seven":
-            console.log("SEVEN WAS CLICKED");
-            break;
-
-        case "eigth":
-            console.log("EIGHT WAS CLICKED");
-            break;
-
-        case "nine":
-            console.log("NINE WAS CLICKED");
-            break;
-
-        case "add":
-            console.log("ADD WAS CLICKED");
-            break;
-        
-        case "minus":
-            console.log("MINUS WAS CLICKED");
-            break;
-
-        case "divide":
-            console.log("DIVIDE WAS CLICKED");
-            break;
-
-        case "multiply":
-            console.log("MULTIPLY WAS CLICKED");
-            break;
-
-        
-        case "clear":
-            console.log("CLEAR WAS CLICKED");
-            break;
-        case "equals":
-            console.log("EQUALS WAS CLICKED");
-            break;
-
-        default:
-            console.log("UH OH!")
-            break;
-    }
-}
-
 let buttons = document.querySelectorAll(".key");
 // Adding event listeners to buttons
 Array.from(buttons).forEach(button => {
-    button.addEventListener('click', function(e) {
-        keyEvent(this.id);
+    button.addEventListener('click', (event) => {
+        console.log(event.target.name);
+        let buttonTarget = event.target;
+        let buttonName = buttonTarget.name;
+        switch (buttonName) {
+            case "number":
+                if(currentNumber == "0" || currentNumber == "") {
+                    currentNumber = buttonTarget.textContent;
+                } else {
+                    currentNumber = currentNumber + buttonTarget.textContent;
+                }
+                break;
+
+            case "symbol":
+                if(symbol == null) {
+                    symbol = buttonTarget.textContent;
+                } else {
+                    operate();
+                    symbol = buttonTarget.textContent;
+                    console.log("New updated symbol" + symbol);
+                }
+
+                break;
+
+            case "clear":
+                currentNumber = "0";
+                previousNumber = null;
+                symbol = null; 
+                break;
+        
+            default:
+                break;
+        }
+        update();
     })
 })
 
